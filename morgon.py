98ea9b7a9d1666689cd0954132aae69cfb83eeb1
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-09-18 09:10:01 +0200

    ok

--- morgon.py
+++ morgon.py
@@ -2,20 +2,20 @@
 
 vaken = "n"
 
-print("Du sover djupt som en björnen i ide...")
+print("Du sover djupt som en björnen i ide...") #skriver ut början på koden
 
-while vaken == "n":
-    vaken = input("vaknar du [y/n]: ").lower()   
+while vaken == "n": #skappar en loop till vaken om användaren svarar nej
+      vaken = input("vaknar du [y/n]: ").lower() #ger användare input till Ja och nej svar medans convertera till minder bokstäver  
 
-print("Du masar dig upp och släpper dig in i dushcen.")
-print("Någon har lämnat en brödråst")
+print("Du masar dig upp och släpper dig in i dushcen.") # nästa steg efter ja svar.
+print("Någon har lämnat en brödråst") #Problem dycker upp och användare ska mötas i val
 
-duscha = input("flyttar du på brödråsten i duschen? [y/n]:  ").lower()
+duscha = input("flyttar du på brödråsten i duschen? [y/n]:  ").lower() #ger användaren svara på ja eller nej inom input.
 
-if duscha == "n": 
+if duscha == "n": #svar för nej visar användaren chockades ijäll och konsolen avslutas
     print("Du tror brödråsten är kul att duscha med en brödråst, men du chockades ijäll")
-    exit()
-elif duscha == "y":
+    exit() # avslutar konsolen
+elif duscha == "y": #Anars
     print("Du valde att flytta brödråsten till köket, och du fortsatte en vanligt dusch")
 else:
   print("Du måste svara en av frågora")
